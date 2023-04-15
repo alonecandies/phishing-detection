69de11633ad8ac44a332a1b839700e74368096a6
alonecandies <dragonfrezze@gmail.com>
2023-04-16 02:03:38 +0700

    query to db to get result and save to db after predict

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ client = MongoClient("mongodb://localhost:27017")
 db = client["phishing"]
 blackList = db["blacklist"]
 whitelist = db["whitelist"]
+dataset = db["dataset_phishing"]
 
 app = Flask(__name__)
 app.config["CACHE_TYPE"] = "null"
@@ -26,70 +27,27 @@ model = None
 with open('./checkpointModel/model_phishing_webpage_classifer', 'rb') as file:
     model = pickle.load(file)
 
-    mappingCriteria = ['length_url',
-                       'length_hostname',
-                       'ip',
-                       'nb_dots',
-                       'nb_qm',
-                       'nb_eq',
-                       'nb_slash',
-                       'nb_www',
-                       'ratio_digits_url',
-                       'ratio_digits_host',
-                       'tld_in_subdomain',
-                       'prefix_suffix',
-                       'shortest_word_host',
-                       'longest_words_raw',
-                       'longest_word_path',
-                       'phish_hints',
-                       'nb_hyperlinks',
-                       'ratio_intHyperlinks',
-                       'empty_title',
-                       'domain_in_title',
-                       'page_rank']
-
-
-# @app.route('/survey', methods=["GET", "POST"])
-# def survey():
-
-#     features = {
-#         'Chứa địa chỉ IP trong URL': 'Các trang web lừa đảo thường không đăng ký tên miền thay vào đó là sử dụng nguyên IP vì vậy hãy cẩn thận',
-#         'Chứa ký tự @ trong URL': 'Dấu @ có tác dụng bỏ quả tất cả ký tự xuất hiện trước nó (VD: http://totally-legit-site.com@192.168.50.20/account sẽ đưa nạn nhân đến trang 192.168.50.20/account là trang web lửa đảo',
-#         'Địa chỉ trang web chứa nhiều path': 'Tìm kiếm điểm chung của trang web lừa đảo giựa vào số đường dẫn có trong url',
-#         'Có ký tự // trong tên miền': 'Ký tự // nằm trong đường dẫn nhằm chuyển hướng người dùng đến trang web lừa đảo',
-#         'HTTPS hoặc HTTP trong tên miền': 'sử dụng https trong domain khiến người dùng nhìn nhầm và chủ quan (VD: http:https://vietcombank.com.vn)',
-#         'Sử dụng địa chỉ rút gọn': 'Sử dụng địa chỉ rút gọn như bit.ly để giấu đi địa chỉ thật sự của trang web lừa đảo',
-#         'Có chứa ký tự - trong domain': 'sử dụng ký tự - trong tên miền khiến tên trang web nhìn "có vẻ" không lừa đảo',
-#         'Kiểm tra xem DNS có nhận được website không': 'Kiểm tra xem DNS có trỏ đến được trang web không, nếu không thì trang web đó được đăng ký với dịch vụ không rõ ràng',
-#         'Tuổi thọ của tên miền có dưới 6 tháng': 'Nhưng trang web lừa đảo thường bị báo cáo liên tục đẫn đến việc gỡ xuống và nhưng tên lừa đảo thường không hay bỏ chi phí duy trì server nên tuổi thọ thường rất ngắn',
-#         'Tên miền đã hết hạn': 'Tên miền đã hết hạn đăng ký',
-#         'Website có sử dụng Iframe': 'Sử dụng Iframe chạy chầm trong các trang web để ăn cắp thông tin cá nhân',
-#         'Website có sử dụng Mouse_Over': 'Sử dụng hàm mouse_over trong javscript để khi người dùng đung đưa con chuột qua 1 cái link bất kỳ trang web lừa đảo sẽ tự động bật lên',
-#         'Website tắt chức năng Right_Click': 'Trang web vô hiệu hóa chuột phải vì lo sợ ta sẽ nhìn thấy những đoạn mã độc trong trang web',
-#         'Sô lần bị forward có quá 2 lần khi vào trang web': 'Khi vào 1 trang web số lần ta bị tự động forward quá nhiều nhằm qua mặt các công cụ quét',
-#         'Địa chỉ Website có chứa punny code': 'Sử dụng punnycode để đánh lừa url (VD: dı sẽ nhìn khá giống với di nhưng punnycode của adıdas.de là trang web lừa đảo với đủ ký tự là http://xn--addas-o4a.de/ nhưng trình duyệt sẽ encode và hiển thị giống như là adidas.de'
-#     }
-#     sublist = [list(features.keys())[n:n+3]
-#                for n in range(0, len(list(features.keys())), 3)]
-#     if request.method == "POST" and request.form['url'] != None:
-#         url = request.form['url']
-
-#         if url == '':
-#             return jsonify({'notvalid': 'Maybe your input not correct'})
-
-#         if (isinstance(url, str)):
-#             url_prepped = preprocess_url(url, tokenizer)
-#             prediction = model.predict(url_prepped)
-
-#             if prediction > 0.5:
-#                 return jsonify({'notsafe': 'Website Phishing ', 'score': str(prediction[0][0])})
-#             else:
-#                 return jsonify({'safe': 'Website Legitimate', 'score': str(prediction[0][0])})
-
-#         # return render_template('index.html',data=sublist,features=features)
-
-#     return render_template('index.html', data=sublist, features=features)
-
+mappingCriteria = ['length_url',
+                   'length_hostname',
+                   'ip',
+                   'nb_dots',
+                   'nb_qm',
+                   'nb_eq',
+                   'nb_slash',
+                   'nb_www',
+                   'ratio_digits_url',
+                   'ratio_digits_host',
+                   'tld_in_subdomain',
+                   'prefix_suffix',
+                   'shortest_word_host',
+                   'longest_words_raw',
+                   'longest_word_path',
+                   'phish_hints',
+                   'nb_hyperlinks',
+                   'ratio_intHyperlinks',
+                   'empty_title',
+                   'domain_in_title',
+                   'page_rank']
 
 @app.route("/feedback", methods=["GET", "POST"])
 def feedback():
@@ -137,6 +95,39 @@ def predict():
                 return jsonify({"predictions": [{'result': 'This website may be safe', 'phishingPercentage': 0, 'url': url}]})
             if blackList.find_one({'url': domain}) is not None:
                 return jsonify({"predictions": [{'result': 'This website may be phishing', 'phishingPercentage': 100, 'url': url}]})
+            # cursor = dataset.find_one({'url': url})
+            # if cursor is not None:
+            #     features = []
+            #     predi=0
+            #     try:
+            #         for i in range(len(mappingCriteria)):
+            #             features.append(cursor[mappingCriteria[i]])
+            #     except:
+            #         pass
+            #     try:
+            #         predi = float(cursor["score"]) * 100
+            #     except:
+            #         predi = float(
+            #             model.predict_proba([features])[0][1]) * 100
+            #     result = ""
+            #     try:
+            #         if (cursor["status"] == "phishing"):
+            #             result = "This website may be phishing"
+            #         else:
+            #             result = "This website may be safe"
+            #     except:
+            #         if (predi > 0.4):
+            #             result = "This website may be phishing"
+            #         else:
+            #             result = "This website may be safe"
+            #     extDetail = {}
+            #     for i in range(len(features)):
+            #         extDetail[mappingCriteria[i]] = features[i]
+            #     r = {"result": result, "phishingPercentage": prediction,
+            #          "url": url, "detail": extDetail}
+            #     data["predictions"].append(r)
+            #     data["success"] = True
+            #     return jsonify(data)
             extractLink = url_extractor(url, page)
             if (extractLink == []):
                 return jsonify({'message': 'Sorry, we can not analyze this URL for now'})
@@ -147,19 +138,25 @@ def predict():
                 result = "This website may be phishing"
             else:
                 result = "This website may be safe"
-            prediction = float(prediction)
-            prediction = prediction * 100
+            predictionPer = float(prediction) * 100
             extDetail = {}
             for i in range(len(extractLink)):
                 extDetail[mappingCriteria[i]] = extractLink[i]
 
-            r = {"result": result, "phishingPercentage": prediction,
+            r = {"result": result, "phishingPercentage": predictionPer,
                  "url": url, "detail": extDetail}
             data["predictions"].append(r)
 
             # Show that the request was a success.
             data["success"] = True
             data["time_elapsed"] = end
+
+            # insertData = {}
+            # insertData["url"] = url
+            # insertData["score"] = prediction
+            # for i in range(len(extractLink)):
+            #     insertData[mappingCriteria[i]] = extractLink[i]
+            # dataset.insert_one(insertData)
 
         else:
             if (isinstance(url, list)):
@@ -190,6 +187,40 @@ def predict():
                         url.remove(newURL)
                         pages.remove(page)
                         continue
+                    # cursor = dataset.find_one({'url': newURL})
+                    # if cursor is not None:
+                    #     features = []
+                    #     predi=0
+                    #     try:
+                    #         for i in range(len(mappingCriteria)):
+                    #             features.append(cursor[mappingCriteria[i]])
+                    #     except:
+                    #         pass
+                    #     try:
+                    #         prediPer = float(cursor["score"]) * 100
+                    #     except:
+                    #         predi = model.predict_proba([features])[0][1]
+                    #         prediPer = float(predi) * 100
+                    #     result = ""
+                    #     try:
+                    #         if (cursor["status"] == "phishing"):
+                    #             result = "This website may be phishing"
+                    #         else:
+                    #             result = "This website may be safe"
+                    #     except:
+                    #         if (predi > 0.4):
+                    #             result = "This website may be phishing"
+                    #         else:
+                    #             result = "This website may be safe"
+                    #     extDetail = {}
+                    #     for i in range(len(features)):
+                    #         extDetail[mappingCriteria[i]] = features[i]
+                    #     r = {"result": result, "phishingPercentage": prediPer,
+                    #          "url": url, "detail": extDetail}
+                    #     data["predictions"].append(r)
+                    #     url.remove(newURL)
+                    #     pages.remove(page)
+                    #     continue
                 if not url == []:
                     def extractURL(i):
                         return url_extractor(url[i], pages[i])
@@ -208,16 +239,22 @@ def predict():
                             result = "This website may be phishing"
                         else:
                             result = "This website may be safe"
-                        prediction = float(pred[0][1])
-                        prediction = prediction * 100
+                        prediPer = float(pred[0][1]) * 100
                         extDetail = {}
                         for j in range(len(listURLExtracted[i])):
                             extDetail[mappingCriteria[j]
                                       ] = listURLExtracted[i][j]
 
-                        r = {"result": result, "phishingPercentage": prediction,
+                        r = {"result": result, "phishingPercentage": prediPer,
                              "url": url[i], "detail": extDetail}
                         data["predictions"].append(r)
+                        # insertData = {}
+                        # insertData["url"] = url[i]
+                        # insertData["score"] = pred[0][1]
+                        # for z in range(len(listURLExtracted[i])):
+                        #     insertData[mappingCriteria[i]
+                        #                ] = listURLExtracted[i][z]
+                        # dataset.insert_one(insertData)
                     end = time.time() - start
                     data["success"] = True
                     data["time_elapsed"] = end
@@ -226,32 +263,34 @@ def predict():
         return jsonify({'message': 'Send me something'})
 
 
-# @app.route("/detail", methods=["GET", "POST"])
-# def detail():
-#     data = {"success": False}
-#     if request.method == "POST":
-#         start = time.time()
-#         incoming = request.get_json()
-#         url = incoming["url"]
+@app.route("/detail", methods=["GET", "POST"])
+def detail():
+    data = {"success": False}
+    if request.method == "POST":
+        start = time.time()
+        incoming = request.get_json()
+        url = incoming["url"]
 
-#         if url == '':
-#             return jsonify({'message': 'Maybe your input not correct'})
+        if url == '':
+            return jsonify({'message': 'Maybe your input not correct'})
 
-#         ext = Extractor()
-#         if (isinstance(url, str)):
-#             extResult = ext(url)
-#             end = time.time() - start
+        if (isinstance(url, str)):
+            isAccessible, newURL, page = is_URL_accessible(url)
+            if not isAccessible:
+                return jsonify({'message': 'Sorry, we can not analyze this URL for now'})
+            extResult = url_extractor(newURL, page)
+            end = time.time() - start
 
-#             # map extdetail with criteria to return as object
-#             extDetail = {}
-#             for i in range(len(extResult)):
-#                 extDetail[mappingCriteria[i]] = extResult[i]
-#             data['detail'] = extDetail
-#             data["success"] = True
-#             data["time_elapsed"] = end
-#         return jsonify(data)
-#     else:
-#         return jsonify({'message': 'Send me something'})
+            # map extdetail with criteria to return as object
+            extDetail = {}
+            for i in range(len(extResult)):
+                extDetail[mappingCriteria[i]] = extResult[i]
+            data['detail'] = extDetail
+            data["success"] = True
+            data["time_elapsed"] = end
+        return jsonify(data)
+    else:
+        return jsonify({'message': 'Send me something'})
 
 
 # Start the server.
